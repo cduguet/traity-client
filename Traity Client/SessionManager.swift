a94b6d531f6653c3ff5c91668498721e5da0f1ec
Cristian <duguet@gmail.comcd>
2015-11-07 20:08:45 -0300

    All first version.

---
+++ Traity Client/SessionManager.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  Traity Client
+//
+//  Created by Cristian Duguet on 11/4/15.
+//  Copyright © 2015 Cristian Duguet. All rights reserved.
+//
+
+
+import UIKit
+import AFNetworking
+import SwiftHTTP
+
+
+class SessionManager : AFHTTPSessionManager {
+    
+    init(baseURL url: NSURL!) {
+        super.init(baseURL: url, sessionConfiguration: nil)
+        requestSerializer = AFJSONRequestSerializer();
+        responseSerializer = AFJSONResponseSerializer();
+    }
+
+    convenience init() {
+        let path = NSBundle.mainBundle().pathForResource("Settings", ofType: "plist")
+        //let host = NSDictionary(contentsOfFile: path!)?.objectForKey("API Host") as! String
+        let host = "api.traity.com"
+        
+        self.init(baseURL:NSURL(string: host))
+    }
+    
+    override func dataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSURLResponse, AnyObject?, NSError?) -> Void)?) -> NSURLSessionDataTask {
+        let req = request as! NSMutableURLRequest
+        let storage = NSUserDefaults.standardUserDefaults()
+        if let accessToken = storage.objectForKey("AccessToken") as? String {
+            req.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+        }
+        return super.dataTaskWithRequest(req, completionHandler: completionHandler)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Traity Client/ViewController.swift
+++ Traity Client/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import AFOAuth2Manager
+import JWT
+
+let traityKey = "tI0c1olhMQTJkJORLBM5yg"
+let traitySecret = "DKHkMa5fMRvDcoqUZpEFamOy9z2kMlAO74FUw"
+let userID = "cristian@crowdtransfer.com"
+
+
 
 class ViewController: UIViewController {
-
+    
+    //var isObserved = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    
+    
+    func widget_signature(key: String, secret: String, current_user_id: String, options: [String:AnyObject] = [:]) -> String {
+        var payload = options as Dictionary
+        payload["time"] = (Int(NSDate().timeIntervalSince1970))
+        payload["current_user_id"] = current_user_id
+        
+        let signature = ["key": key, "payload": JWT.encode(payload, algorithm: .HS256(secret))]
+        return JWT.encode(signature, algorithm: .None)
+    }
+    
+    
+    @IBAction func authenticate(sender: AnyObject) {
+        
+        
+        let sign = widget_signature(traityKey, secret: traitySecret, current_user_id: userID)
+        let redirect_uri = "com.swapp.traityclient:/oauth2Callback"
+        // 3 calculate final url
+        let params = "?signature=\(sign)&success=\(redirect_uri)"
+        
+        print("https://traity.com/apps/connect\(params)")
+        UIApplication.sharedApplication().openURL(NSURL(string: "https://traity.com/apps/connect\(params)")!)
+       
+    }
+    
+  
+    @ IBAction func getReputation(sender: AnyObject) {
+        let manager = SessionManager()
+        
+        manager.GET("/1.0/app-users/\(userID)", parameters: nil, success: { (task, response) -> Void in
+            print(response)
+            }, failure: { (task, response) -> Void in
+                print("failed!")
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +68,3 @@ class ViewController: UIViewController {
 
 
 }
-
